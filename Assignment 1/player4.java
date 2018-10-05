ccb618b666f701a1d1ec136383056be36da4d457
Sietse Molenaar <sietse.molenaar@student.uva.nl>
2018-10-05 15:28:08 +0200

    Fixed tournament function for individual class.

--- Assignment 1/player4.java
+++ Assignment 1/player4.java
@@ -4,28 +4,29 @@ import org.vu.contest.ContestEvaluation;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Properties;
-import java.util.List;
-import java.util.stream.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.stream.*;
+
 
 public class player4 implements ContestSubmission {
     public static Random rnd_;
     public static ContestEvaluation evaluation;
     private int evaluations_limit_;
     // Population size
-    private final int populationSize = 100;
+    private final int populationSize = 20;
     private final int F_DIMENSIONS = 10;
-    private List<Individual> population;
+    private LinkedList<Individual> population;
     private double selectivePressure = 1.5;
     private int tournamentSize = 5;
     private double mutationRate = 0.1;
     private double mutationVariability = 0.5;
     private int hardElitismN = 1;
-    private int tot = 0;
+    private int tot=0;
+
 
     /**
      * Initialize the popoulation randomly
@@ -42,21 +43,10 @@ public class player4 implements ContestSubmission {
         populationInitialization();
     }
 
-    
-    //to write better which type of mutation it is
-    private void mutateChild(double premuChild[]) {
-        for (int i = 0; i < premuChild.length; i++) {
-            double coinFlip = rnd_.nextDouble();
-            if (coinFlip > 0.5) {
-                premuChild[i] = (rnd_.nextDouble() * 10) - 5;
-            }
-        }
-    }
-
     //same as before
     private void mutateChild2(double premuChild[]){
         // Mutation that will move shortly in the 10D space (Low variation)
-        double p1 = 0.8 * mutationVariability + 0.1; // p1 from 0.1 to 0.9
+        double p1 = 0.8 * mutationVariability + 0.1; //p1 from 0.1 to 0.9
         // Move some random values a random % distance towards one of the sides (+ or -)
         for (int i = 0; i < premuChild.length; i++) {
             double coinFlip = rnd_.nextDouble();
@@ -66,7 +56,7 @@ public class player4 implements ContestSubmission {
                 double maxDistance = 0.0;
                 if (movePositive) {
                     maxDistance = 5.0 - premuChild[i];
-                } else {
+                } else{
                     maxDistance = -Math.abs(-5.0 - premuChild[i]);
                 }
 
@@ -78,8 +68,7 @@ public class player4 implements ContestSubmission {
     /**
      * 
      * @param wrappers larray of Wrapper containing the population
-     * @return Returns the top hardElitismN individuals directly to next generation
-     *         (array of hardElitismN indivs)
+     * @return Returns the top hardElitismN individuals directly to next generation (array of hardElitismN indivs)
      */
     public Wrapper[] hardElitism(Wrapper[] wrappers) {
         Arrays.sort(wrappers);
@@ -135,40 +124,40 @@ public class player4 implements ContestSubmission {
     /**
      * 
      * @param position order of arrival of the parent within the tournament
-     * @param u        the dimension of the tournament
+     * @param u the dimension of the tournament
      * @return the probability for the parent to be chosen
      */
-    private double linearRanking(int position, int u) {
+    private double linearRanking(int position,int u) {
         return ((2 - selectivePressure) / u) + ((2 * position * (selectivePressure - 1)) / (u * (u - 1)));
     }
 
     /**
      * 
-     * @param n   # of random number generated
+     * @param n # of random number generated
      * @param min min for every number
      * @param max max for every number
-     * @return n integer random number between min and max
+     * @return  n integer random number between min and max
      */
     private int[] random(int n, int min, int max) {
         int[] res = new int[n];
         Random random = new Random();
-        int[] i = new int[] { 0 };
+        int[] i = new int[]{0};
         random.ints(n, min, max).forEach(rn -> {
             res[i[0]++] = rn;
         });
         return res;
     }
 
-    private List<Wrapper> tournament(int tournamentSize, int winners, int rounds) {
-        List<Wrapper> res = new LinkedList<Wrapper>();
-        List<Wrapper> candidates = (List) population.clone();
+    private List<Individual> tournament(int tournamentSize, int winners, int rounds) {
+        List<Individual> res = new LinkedList<Individual>();
+        List<Individual> candidates = (List) population.clone();
 
         for (int r = 0; r < rounds; r++) {
             // select randomly the index of 5 parents
             List<Integer> parents = IntStream.of(random(tournamentSize, 0, candidates.size() - 1)).boxed()
                     .collect(Collectors.toList());
 
-            List<Wrapper> tournament = new LinkedList<Wrapper>();
+            List<Individual> tournament = new LinkedList<Individual>();
             Collections.sort(parents);
 
             // add all selected parents to tournament
@@ -176,7 +165,7 @@ public class player4 implements ContestSubmission {
             int iteration = 0;
             while (can.hasNext() && p.hasNext()) {
                 if (iteration == (Integer) p.next())
-                    tournament.add((Wrapper) can.next());
+                    tournament.add((Individual) can.next());
                 else
                     can.next();
                 iteration++;
@@ -185,21 +174,21 @@ public class player4 implements ContestSubmission {
             for (int w = 0; w < winners; w++) {
                 // calculate the total fitness of the tournament
                 double totalFitness = 0;
-                for (Wrapper candidate : tournament)
-                    totalFitness += (double) candidate.c;
+                for (Individual candidate : tournament)
+                    totalFitness += candidate.getFitness();
 
                 // calculate the probability for every parent to be chosen, the sum is 1.0
                 double[] probability = new double[tournamentSize];
                 iteration = 0;
-                for (Wrapper candidate : tournament)
-                    probability[iteration++] = (double) candidate.c / totalFitness;
+                for (Individual candidate : tournament)
+                    probability[iteration++] = candidate.getFitness() / totalFitness;
 
                 double amount = probability[0];
                 // randomize a number between 0 and 1
                 double extract = Math.random();
                 for (int i = 1; i <= probability.length; i++)
                     if (extract <= amount) {
-                        Wrapper winner = tournament.get(i);
+                        Individual winner = tournament.get(i);
                         // add the winner to the outcome
                         res.add(winner);
                         // remove the winner for next round within the same tournament
@@ -215,59 +204,61 @@ public class player4 implements ContestSubmission {
         return res;
     }
 
+
     public void run() {
         // Run your algorithm here
         int evals = 0;
-        while (evals++ * populationSize < 10000) {
-            // calculate fitness
-            Wrapper[] popFitness = computeFitness();
-            double totalFitness = sumFitness(popFitness);
-
-            for (int counter = 0; counter < populationSize; counter++) {
-                // select randomly the index of 5 parents
-                int[] parents = random(tournamentSize, 0, populationSize - 1);
-                // calculate the probability for every parent to be chosen, the sum is 1.0
+        while (evals++*populationSize < 10000) {
+            double totalFitness = sumFitness();
+            
+            System.out.println(tot);
+            System.out.println(population.size());
+            for (int counter=0;counter<populationSize;counter++) {
+                //select randomly the index of 5 parents
+                int[] parents = random(tournamentSize, 0, populationSize-1);
+                //calculate the probability for every parent to be chosen, the sum is 1.0
                 double[] probability = new double[tournamentSize];
-                for (int i = 0; i < tournamentSize; i++) {
-                    probability[i] = linearRanking(i, tournamentSize);
+                for (int i=0;i<tournamentSize;i++) {
+                    probability[i] = linearRanking(i,tournamentSize);
                 }
-                Wrapper p1=null, p2=null;
+                Individual p1 = null, p2 = null;
                 double amount = probability[0];
-                // randomize a number between 0 and 1
-                double extract = Math.random();
-
-                // select the parents according to the random number and the probability of the
-                // parents
-                for (int i = 1; i <= probability.length; i++)
+                //randomize a number between 0 and 1
+                double extract = Math.random(); 
+                
+                //select the parents according to the random number and the probability of the parents
+                for(int i = 1; i <= probability.length; i++)
                     if (extract <= amount) {
-                        p1 = popFitness[parents[i - 1]];
+                        p1 = population.get(parents[i-1]);
                         break;
                     } else {
                         amount += probability[i];
                     }
-                // repeat the procedure for the second parent
+                //repeat the procedure for the second parent
                 extract = Math.random();
-
+                
                 amount = probability[0];
-                for (int i = 1; i <= probability.length; i++)
+                for(int i=1;i<=probability.length;i++)
                     if (extract <= amount) {
-                        p2 = popFitness[parents[i-1]];
+                        p2 = population.get(parents[i-1]);
                         break;
                     } else {
                         amount += probability[i];
                     }
-                // generation of the new child from the parents
-                double[] newChild = crossover((double[]) p1.t, (double[]) p2.t);
-                // apply the mutation if it necessary
-                if (Math.random() < mutationRate)
-                    mutateChild(newChild);
-                // add the new child to the population
+                //generation of the new child from the parents
+                Individual newChild = crossover(p1, p2);
+
+                //apply the mutation if it necessary
+                if(rnd_.nextDouble() < mutationRate)
+                    newChild.mutate();
+
+                //add the new child to the population
                 population.add(newChild);
             }
             //population selection, half of the individuals must be killed
             //inefficient way, just to understand if everything works properly
             Collections.sort(population);
-            population = population.subList(0, population.size()/2);
+            population = (LinkedList) population.subList(0, population.size()/2);
         }
     }
 }
