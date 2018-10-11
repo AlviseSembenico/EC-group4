f3e3b26b938e2e05b301ca568c283ddbc3aae27f
Sietse Molenaar <sietse.molenaar@student.uva.nl>
2018-10-11 14:01:01 +0200

    Format Cluster.java.

--- Assignment 1/EC4/src/ec4/Cluster.java
+++ Assignment 1/EC4/src/ec4/Cluster.java
@@ -9,86 +9,87 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- *
  * @author alvis
  */
 public class Cluster {
     public List<Individual> components;
     public List<Double> fitnessHistory;
-    
-    public Cluster(){
-        components=new LinkedList<Individual>();
-        fitnessHistory=new LinkedList<Double>();
+
+    public Cluster() {
+        components = new LinkedList<Individual>();
+        fitnessHistory = new LinkedList<Double>();
     }
-    public Cluster(List<Individual> l){
-        components=l;
+
+    public Cluster(List<Individual> l) {
+        components = l;
     }
-    public Cluster(Individual i){
+
+    public Cluster(Individual i) {
         this();
         components.add(i);
     }
-    
-    public double fitnessMean(){
-        double mean=0;
-        for(Individual i:components)
-            mean+=i.getFitness();
-        
-        mean/=components.size();
+
+    public double fitnessMean() {
+        double mean = 0;
+        for (Individual i : components)
+            mean += i.getFitness();
+
+        mean /= components.size();
         fitnessHistory.add(mean);
         return mean;
     }
-    
-    public double fitnessVariance(){
-        double tot=0;
-        double mean=fitnessMean();
-        for(Individual i:components)
-            tot+=Math.pow(i.getFitness()-mean,2);
-        
-        tot/=components.size();
-        tot=Math.sqrt(mean);
+
+    public double fitnessVariance() {
+        double tot = 0;
+        double mean = fitnessMean();
+        for (Individual i : components)
+            tot += Math.pow(i.getFitness() - mean, 2);
+
+        tot /= components.size();
+        tot = Math.sqrt(mean);
         return tot;
     }
-    
-    public double radius(){
-        double max=0;
-        for(Individual i:components)
-            for(Individual j:components){
-                double dist=i.distance(j);
-                if(dist>max)
-                    max=dist;
+
+    public double radius() {
+        double max = 0;
+        for (Individual i : components)
+            for (Individual j : components) {
+                double dist = i.distance(j);
+                if (dist > max)
+                    max = dist;
             }
         return max;
     }
-    
-    public double maxDistance(Cluster cl){
-        if(this==cl)
+
+    public double maxDistance(Cluster cl) {
+        if (this == cl)
             return 0;
-        double max=0;
-        for(Individual i:components)
-            for(Individual j:cl.components){
-            double dist=i.distance(j);
-                if(dist>max)
-                    max=dist;
+        double max = 0;
+        for (Individual i : components)
+            for (Individual j : cl.components) {
+                double dist = i.distance(j);
+                if (dist > max)
+                    max = dist;
             }
-        if(components.size()+cl.components.size()==0)
+        if (components.size() + cl.components.size() == 0)
             return 0;
         return max;
     }
-    
-    public double averageDistance(Cluster cl){
-        if(this==cl)
+
+    public double averageDistance(Cluster cl) {
+        if (this == cl)
             return 0;
-        double tot=0;
-        for(Individual i:components)
-            for(Individual j:cl.components)
-                tot+=i.distance(j);
-        if(components.size()+cl.components.size()==0)
+        double tot = 0;
+        for (Individual i : components)
+            for (Individual j : cl.components)
+                tot += i.distance(j);
+        if (components.size() + cl.components.size() == 0)
             return 0;
-        return tot/(components.size()+cl.components.size());
+        return tot / (components.size() + cl.components.size());
     }
-    
-    public void addIndividual(Individual i){
+
+    public void addIndividual(Individual i) {
         components.add(i);
     }
-    
+
 }
